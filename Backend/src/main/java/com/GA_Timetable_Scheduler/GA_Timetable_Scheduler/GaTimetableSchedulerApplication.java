bacf8c13dcad4f7ef9635c80751913fa348d124e
DangQuangTien <thintien0@gmail.com>
2026-07-06 19:01:51 +0700

    chore: initialize Spring Boot project setup

---
+++ Backend/src/main/java/com/GA_Timetable_Scheduler/GA_Timetable_Scheduler/GaTimetableSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.GA_Timetable_Scheduler.GA_Timetable_Scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaTimetableSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaTimetableSchedulerApplication.class, args);
+	}
+
+}
